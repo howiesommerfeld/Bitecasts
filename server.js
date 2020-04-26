e8c8be398dea044125068b6f5ac1292f7f253999
howiesommerfeld <howiesommerfeld@gmail.com>
2020-04-26 20:11:22 +0200

    Boilerplate Express server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const server = require('http').Server(app);
+
+const bodyParser = require('body-parser');
+const errorHandler = require('errorhandler');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(errorHandler());
+app.use(morgan('dev'));
+
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+
+app.listen(port, () => {
+    console.log('listening on *:' + port);
+  });
