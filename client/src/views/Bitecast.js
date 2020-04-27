de7ae63d62bd5b3aed435540d224e2de1f46aa16
howiesommerfeld <howiesommerfeld@gmail.com>
2020-04-27 21:37:36 +0200

    Initial setup of bitecast playback page

--- client/src/views/Bitecast.js
+++ client/src/views/Bitecast.js
@@ -1,37 +1,89 @@
 import React, { Component } from 'react';
-
+import Loader from 'react-loader-spinner';
 
 class Bitecast extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-          bitecast: {}
+          bitecast: {},
+          isLoading: true
         };
       }
 
-  componentDidMount(){
-      this.getBitecast()
-      .then((json)=>{
-          this.setState({bitecast:json["bitecast"]});
-      })
-  }
+  componentDidMount() {
 
-  getBitecast(){
-    return fetch(this.props.location.pathname, {
+    const linkedState = this.props.location.state;
+    if(linkedState) {
+        this.setState({
+            bitecast:linkedState.bitecast,
+            isLoading: false
+      });
+      return;
+    }
+
+
+    fetch(this.props.location.pathname, {
         accept: "application/json"
       })
-      .then((response)=>{return response.json()});
+      //TODO Handle 404 elegantly here:
+      .then(res => res.json())
+      .then((json)=>{
+          this.setState({
+              bitecast:json["bitecast"],
+              isLoading: false
+        });
+      })
+      .catch( error => {
+        console.log(error);
+      })
   }
 
-
   render() {
+
+    const loadingIndicator = (
+        <div>
+            <h2>Loading Bitecast...</h2>    
+            <div style={{width: "100%",height: "100",display: "flex",justifyContent: "center",alignItems: "center"}}>
+                
+                <Loader type="ThreeDots" color="#2BAD60" height="100" width="100" />
+            </div>
+        </div>
+        
+    );
+        
+    //TODO: Refacotr into it's own component & style
+    const bitecast = (
+        <div className="bitecast item">
+            <div className="bitecast title">
+                Title: {this.state.bitecast.title}
+            </div>
+            <div className="bitecast image">
+                <div>Cover Image:</div>
+                <img alt="Bitecast cover"src={this.state.bitecast.coverImageLink} height="100" width="100"></img>
+            </div>
+            <div className="bitecast creator">
+                Creator: {this.state.bitecast.creatorName}
+            </div>
+            <div className="bitecast categories">
+
+            </div>
+            <div className="bitecast audio">
+                <audio controls>
+                    <source src={this.state.bitecast.audioLink} type="audio/aac"/>
+                </audio>
+            </div>
+        </div>
+    );
+
+    const currentBitecast = ( 
+        this.state.isLoading === true ? <div> {loadingIndicator} </div> : <div> {bitecast} </div>
+     );
+        
+
     return (
       <div className="Bitecast">
-        <h2>LISTEN TO BITECAST</h2>
-        <div className="bitecast item">
-          {this.state.bitecast.title}
-        </div>
+        {currentBitecast}
       </div>
     );
   }
