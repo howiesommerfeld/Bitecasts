8c54bf067c70418bd1bed933403fdafca6f3ffeb
howiesommerfeld <howiesommerfeld@gmail.com>
2020-04-27 10:55:58 +0200

    Add routes for landing and bitecast pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Link, HashRouter as Router, Route } from 'react-router-dom';
+import Landing from './views/Landing';
+import Bitecast from './views/Bitecast';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+        <Link to="/" className="logo">
+          <img src={logo} className="App-logo" alt="logo" />
+        </Link>
+        </header>
+        <Route exact path="/" component={Landing} />
+        <Route path="/api/bitecasts/:id" component={Bitecast} />
+  {/*<Route path="/series/:id" component={Series} />*/}
+      </div>
+    </Router>
   );
 }
 
