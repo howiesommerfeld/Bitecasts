f1ba282c6ced25479ad90dac0beeb414a1a1ba49
howiesommerfeld <howiesommerfeld@gmail.com>
2020-04-28 10:39:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -18,12 +18,11 @@ app.use(morgan('dev'));
 require('./routes')(app);
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-/*if(process.env.NODE_ENV === 'production') {  
-    app.use(express.static(path.join(__dirname, 'client/build')));  
-    //app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));})
+if(process.env.NODE_ENV === 'production') {  
+    app.get('/', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));})
 } else {
-   // app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/client/public/index.html'));})
-}*/
+    app.get('/', (req, res) => {  res.sendFile(path.join(__dirname+'/client/public/index.html'));})
+}
 
 
 
